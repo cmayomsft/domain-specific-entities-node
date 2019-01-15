1692831c3214c0562672897f6330b11e79795a66
Drew Marsh <drmarsh@microsoft.com>
2019-01-15 08:21:33 -0800

    More work on fleshing out LUIS intent recognizer

--- packages/luis-integration/src/index.ts
+++ packages/luis-integration/src/index.ts
@@ -3,6 +3,17 @@ import { BasicEntity, IIntentRecognizer } from "conversation-processor";
 
 export interface LuisEntity extends BasicEntity {
     readonly type: "luis";
+    readonly score?: number;
+}
+
+export interface LuisCompositeEntity extends LuisEntity {
+    readonly luisType: "composite";
+    readonly children: LuisEntity[];
+}
+
+export interface LuisEntityWithResolution extends LuisEntity {
+    readonly luisType: "builtin";
+    readonly resolution: any;
 }
 
 export function createLuisIntentRecognizer<TConversationContext>(luisClient: LUISRuntimeClient, appId: string, luisPredictionResolveOptions?: LUISRuntimeModels.PredictionResolveOptionalParams): IIntentRecognizer<TConversationContext, LuisEntity> {
@@ -24,13 +35,35 @@ export function createLuisIntentRecognizer<TConversationContext>(luisClient: LUI
     };
 }
 
-function mapEntity(luisEntity: LUISRuntimeModels.EntityModel): LuisEntity {
-    return {
+function mapEntity(luisEntity: LUISRuntimeModels.EntityModel): LuisEntity|LuisCompositeEntity|LuisEntityWithResolution {
+    const baseEntity: LuisEntity = {
         name: luisEntity.entity,
         type: "luis",
         utteranceOffsets: {
             startIndex: luisEntity.startIndex,
             endIndex: luisEntity.endIndex,
         },
+        score: luisEntity.score,
     };
+
+    switch (luisEntity.type) {
+        case "composite":
+            return {
+                ...baseEntity,
+                luisType: "composite",
+                children: [],
+            };
+
+        case "builtin":
+            return {
+                ...baseEntity,
+                luisType: "builtin",
+                resolution: (luisEntity as LUISRuntimeModels.EntityWithResolution).resolution,
+            };
+
+        default:
+            return baseEntity;
+    }
+
+
 }
