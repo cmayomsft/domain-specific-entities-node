7b5a2e679b12412e6ed4d6935917fe9194234eff
Drew Marsh <drmarsh@microsoft.com>
2019-04-15 10:51:07 -0700

    Update CLI sample configs to latest schema
    Forgot to update the configuration files `./samples` for the CLI when the schema
    change happened. This brings those up to spec.

--- packages/intentalyzer-cli/samples/basic-configuration.js
+++ packages/intentalyzer-cli/samples/basic-configuration.js
@@ -1,20 +1,29 @@
 module.exports = {
-    recognizers: [ {
-        "recognize": function(context, utterance) {
-            return Promise.resolve({
-                utterance: utterance,
-                intent: "configured-intent",
-                entities: [],
-            });
-        }
-    }],
-    enrichers: [ {
-        "enrich": function(context, recognizedUtterance) {
-            recognizedUtterance.entities.push({
-                name: "enrichedEntity",
-                type: "string",
-                value: `hello ${Date.now()}`
-            });
-        }
-    }]
-}
+    resolver: {
+        recognizers: [
+            {
+                recognize: function(context, utterance) {
+                    return Promise.resolve({
+                        utterance: utterance,
+                        intent: "configured-intent",
+                        entities: [],
+                    });
+                },
+            },
+        ],
+        enrichers: [
+            {
+                enrich: function(context, recognizedUtterance) {
+                    return { 
+                        ...recognizedUtterance, 
+                        entities: [...recognizedUtterance.entities, {
+                            name: "enrichedEntity",
+                            type: "string",
+                            value: `hello ${Date.now()}`,
+                        } ]
+                    };
+                },
+            },
+        ],
+    },
+};
